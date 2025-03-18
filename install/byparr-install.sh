099b32d69a174cf41f7868a28f596e3c28a16763
Tanuj Dargan <dargantanuj@gmail.com>
2025-03-17 18:12:00 -0700

    Update byparr-install.sh

--- install/byparr-install.sh
+++ install/byparr-install.sh
@@ -5,6 +5,7 @@
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/ThePhaseless/Byparr/
 
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
 color
 verb_ip6
 catch_errors
